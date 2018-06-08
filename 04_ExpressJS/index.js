af62d8f9726aaaca8dfa066805060844340c8380
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-06-08 18:53:35 +0300

    starting express js lab and exercises

---
+++ 04_ExpressJS/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+let app = express()
+const port = 1337
+
+app.get('/', (req, res) => {
+  res.send('Hello from express!')
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
